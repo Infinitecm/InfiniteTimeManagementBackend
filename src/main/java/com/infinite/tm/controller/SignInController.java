13dfa124915d31d22e2363923396ffcb7296d1fa
jairajendran <72862690+jairajendran@users.noreply.github.com>
2021-03-25 17:12:06 +0530

    Update SignInController.java

--- src/main/java/com/infinite/tm/controller/SignInController.java
+++ src/main/java/com/infinite/tm/controller/SignInController.java
@@ -20,7 +20,7 @@ import com.infinite.tm.util.EmailUtility;
 import com.infinite.tm.util.GeneratePassword;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://172.16.21.12:4200")
 @RequestMapping("/TM")
 public final class SignInController {
 	
